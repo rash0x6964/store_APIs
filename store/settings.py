bdbf8c8d913e075e91b61c473856bc8bc73422c5
rash0x6964 <rash0x6964@gmail.com>
2024-10-04 18:19:19 +0100

    Setting up the database

--- store/settings.py
+++ store/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +44,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'store_app',
     'tags_app',
+    'likes_app',
 ]
 
 MIDDLEWARE = [
@@ -79,8 +84,11 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'store',
+        'HOST': 'localhost',
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD')
     }
 }
 
