9cc98cdc1a6929af828a53d885714b38a65a960d
rash0x6964 <rash0x6964@gmail.com>
2024-10-06 14:50:32 +0100

    Add a validation to the product model

--- store_app/models.py
+++ store_app/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(null=True)
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True)
+    unit_price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(1)]
+    )
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey('Collection', on_delete=models.PROTECT)
-    promotions = models.ManyToManyField('Promotion')
+    promotions = models.ManyToManyField('Promotion', blank=True)
 
     def __str__(self) -> str:
         return self.title
@@ -62,7 +67,7 @@ class Collection(models.Model):
 
     def __str__(self) -> str:
         return self.title
-    
+
     class Meta:
         ordering = ['title']
 
