b83f2428d59e09d6bb3b0c6a49d56f4021931584
rash0x6964 <rash0x6964@gmail.com>
2024-10-06 13:32:06 +0100

    Config the admin site && Register the models needed

--- store_app/models.py
+++ store_app/models.py
@@ -4,11 +4,17 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(null=True)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey('Collection', on_delete=models.PROTECT)
     promotions = models.ManyToManyField('Promotion')
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        ordering = ['title']
 
 class Customer(models.Model):
     MEMBERSHIP_CHOICES = [
@@ -23,14 +29,20 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default='B')
 
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
+
 class Order(models.Model):
-    PYMENT_STATUS = [
+    PAYMENT_STATUS = [
         ('P', 'Pending'),
         ('C', 'Complete'),
         ('F', 'Failed'),
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
-    pyment_status = models.CharField(max_length=1, choices=PYMENT_STATUS, default='P')
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS, default='P')
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
 class OrderItem(models.Model):
@@ -47,6 +59,12 @@ class Address(models.Model):
 class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, related_name='+')
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
 
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
