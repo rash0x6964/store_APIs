08762e5170c1aa24714e42b7dd587defa718e5aa
rash0x6964 <rash0x6964@gmail.com>
2024-10-08 00:09:14 +0100

    Build product API list/create

--- store_app/views.py
+++ store_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def product_list(request):
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(
+            queryset,
+            many=True,
+            context={'request': request}
+        )
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+
+@api_view()
+def product_detail(request, id):
+    return Response(id)
