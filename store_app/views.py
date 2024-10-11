181ecb8a021d6aa0b3b210e98f0abe7df7158ac8
rash0x6964 <rash0x6964@gmail.com>
2024-10-11 15:41:47 +0100

    Build the reviews API

--- store_app/views.py
+++ store_app/views.py
@@ -4,8 +4,8 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
-from .serializers import ProductSerializer, CollectionSerializer
-from .models import Product, Collection, OrderItem
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+from .models import Product, Collection, OrderItem, Review
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
@@ -35,3 +35,11 @@ class CollectionViewSet(ModelViewSet):
             )
         return super().destroy(request, *args, **kwargs)
 
+class ReviewViewSet(ModelViewSet):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+    
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
