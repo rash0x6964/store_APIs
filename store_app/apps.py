92375b00e74d8bc0bb24f6245841447d6670e7c4
rash0x6964 <rash0x6964@gmail.com>
2024-10-14 14:51:54 +0100

    Use signals to create a customer profile when a user created

--- store_app/apps.py
+++ store_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class StoreAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store_app'
+
+    def ready(self) -> None:
+        import store_app.signals
